2a6610ed5c21ffb78dde7c8cb6b55a6f5bfe1668
amsm <amsm7437@gmail.com>
2025-05-23 05:09:03 -0700

    Add project files.

---
+++ Mindful/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mindful.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
